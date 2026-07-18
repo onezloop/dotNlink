1d1fd1a7f2df4df5aebfabda6fdbb2bf9b73d205
onezloop <praveen@onezloop.com>
2026-07-18 23:32:44 +0530

    Delete src directory

--- src/utils/time.ts
+++
@@ -1,7 +0,0 @@
-export function formatElapsed(ms: number): string {
-  const totalSeconds = Math.floor(ms / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-  const tenths = Math.floor((ms % 1000) / 100);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}.${tenths}`;
-}
